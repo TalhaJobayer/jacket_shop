130c13c264a42ed564c6a10365c0028579c664a3
talha jobayer <talhajobayer8@gmail.com>
2023-08-10 22:51:49 +0600

    card responsive done

--- src/Component/MarketPlaceProduct/MarketPlaceProduct.js
+++ src/Component/MarketPlaceProduct/MarketPlaceProduct.js
@@ -8,8 +8,8 @@ import {BsFillCartPlusFill} from 'react-icons/bs'
 
 const MarketPlaceProduct = () => {
     return (
-        <div className='marketPlaceContainer grid grid-cols-6 gap-x-10 gap-y-7 m-5    '>
-            <div className="card card-compact w-50 bg-base-100 shadow-xl">
+        <div className='marketPlaceContainer grid lg:grid-cols-6 lg:gap-x-10 lg:gap-y-7 sm:grid-cols-3 sm:gap-x-5 sm:gap-y-3 md:grid-cols-4 md:gap-x-5 gap-y-3 m-5    '>
+            <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
   <figure><img src={photo1} alt="Shoes" /></figure>
   <div className="">
     <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
@@ -22,7 +22,40 @@ const MarketPlaceProduct = () => {
   </div>
 </div>
 {/* ========================================= */}
-<div className="card card-compact w-50 bg-base-100 shadow-xl">
+<div className="card card-compact md:w-50  bg-base-100 shadow-xl">
+  <figure><img src={photo2} alt="Shoes" /></figure>
+  <div className="">
+    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
+    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
+    <h3 className='font-bold text-xl mt-3'>1090</h3>
+    <div className="parentButton flex mt-5 ">
+      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
+      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
+    </div>
+  </div>
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
+  <figure><img src={photo3} alt="Shoes" /></figure>
+  <div className="">
+    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
+    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
+    <h3 className='font-bold text-xl mt-3'>1090</h3>
+    <div className="parentButton flex mt-5 ">
+      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
+      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
+    </div>
+  </div>
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
+  <figure><img src={photo2} alt="Shoes" /></figure>
+  <div className="">
+    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
+    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
+    <h3 className='font-bold text-xl mt-3'>1090</h3>
+    <div className="parentButton flex mt-5 ">
+      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
+      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
+    </div>
+  </div>
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
   <figure><img src={photo1} alt="Shoes" /></figure>
   <div className="">
     <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
@@ -33,7 +66,40 @@ const MarketPlaceProduct = () => {
       <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
     </div>
   </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
+  <figure><img src={photo3} alt="Shoes" /></figure>
+  <div className="">
+    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
+    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
+    <h3 className='font-bold text-xl mt-3'>1090</h3>
+    <div className="parentButton flex mt-5 ">
+      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
+      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
+    </div>
+  </div>
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
+  <figure><img src={photo2} alt="Shoes" /></figure>
+  <div className="">
+    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
+    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
+    <h3 className='font-bold text-xl mt-3'>1090</h3>
+    <div className="parentButton flex mt-5 ">
+      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
+      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
+    </div>
+  </div>
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
+  <figure><img src={photo3} alt="Shoes" /></figure>
+  <div className="">
+    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
+    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
+    <h3 className='font-bold text-xl mt-3'>1090</h3>
+    <div className="parentButton flex mt-5 ">
+      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
+      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
+    </div>
+  </div>
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
   <figure><img src={photo1} alt="Shoes" /></figure>
   <div className="">
     <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
@@ -44,8 +110,8 @@ const MarketPlaceProduct = () => {
       <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
     </div>
   </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
-  <figure><img src={photo1} alt="Shoes" /></figure>
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
+  <figure><img src={photo3} alt="Shoes" /></figure>
   <div className="">
     <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
     <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
@@ -55,8 +121,8 @@ const MarketPlaceProduct = () => {
       <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
     </div>
   </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
-  <figure><img src={photo1} alt="Shoes" /></figure>
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
+  <figure><img src={photo2} alt="Shoes" /></figure>
   <div className="">
     <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
     <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
@@ -66,74 +132,8 @@ const MarketPlaceProduct = () => {
       <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
     </div>
   </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
-  <figure><img src={photo1} alt="Shoes" /></figure>
-  <div className="">
-    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
-    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
-    <h3 className='font-bold text-xl mt-3'>1090</h3>
-    <div className="parentButton flex mt-5 ">
-      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
-      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
-    </div>
-  </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
-  <figure><img src={photo1} alt="Shoes" /></figure>
-  <div className="">
-    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
-    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
-    <h3 className='font-bold text-xl mt-3'>1090</h3>
-    <div className="parentButton flex mt-5 ">
-      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
-      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
-    </div>
-  </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
-  <figure><img src={photo1} alt="Shoes" /></figure>
-  <div className="">
-    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
-    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
-    <h3 className='font-bold text-xl mt-3'>1090</h3>
-    <div className="parentButton flex mt-5 ">
-      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
-      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
-    </div>
-  </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
-  <figure><img src={photo1} alt="Shoes" /></figure>
-  <div className="">
-    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
-    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
-    <h3 className='font-bold text-xl mt-3'>1090</h3>
-    <div className="parentButton flex mt-5 ">
-      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
-      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
-    </div>
-  </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
-  <figure><img src={photo1} alt="Shoes" /></figure>
-  <div className="">
-    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
-    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
-    <h3 className='font-bold text-xl mt-3'>1090</h3>
-    <div className="parentButton flex mt-5 ">
-      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
-      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
-    </div>
-  </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
-  <figure><img src={photo1} alt="Shoes" /></figure>
-  <div className="">
-    <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
-    <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
-    <h3 className='font-bold text-xl mt-3'>1090</h3>
-    <div className="parentButton flex mt-5 ">
-      <button className="btn custom_button  rounded-none  rounded-l-lg flex-auto mr-1 w-50">Buy Now</button>
-      <button className="btn custom_button rounded-none rounded-r-lg flex-auto    w-50"><BsFillCartPlusFill></BsFillCartPlusFill></button>
-    </div>
-  </div>
-</div> <div className="card card-compact w-50 bg-base-100 shadow-xl">
-  <figure><img src={photo1} alt="Shoes" /></figure>
+</div>   <div className="card card-compact md:w-50  bg-base-100 shadow-xl">
+  <figure><img src={photo3} alt="Shoes" /></figure>
   <div className="">
     <h2 className="text-xl font-semibold">Mens Premium Jacket </h2>
     <h4 className='text-md text-slate-400 font-medium mt-2'>Starts From</h4>
